5e3fb45a50adde2a0443fbf630468312da632534
Lucie Pellier <lucie@luciepellier.com>
2023-07-18 11:12:20 +0200

    TDD test add Apartment passed

--- project_immo/gestion_immo/models.py
+++ project_immo/gestion_immo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Apartment(models.Model):
+    address = models.CharField(max_length=200)
+    address_complement = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=10)
+    rental_price = models.DecimalField(max_digits=10, decimal_places=2)
+    charges_price = models.DecimalField(max_digits=10, decimal_places=2)
+    deposit_price = models.DecimalField(max_digits=10, decimal_places=2)
--- project_immo/gestion_immo/tests.py
+++ project_immo/gestion_immo/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from .models import Apartment
 
 # Create your tests here.
 
 class ApartmentTest(TestCase):
     def test_add_apartment(self):
         # Test in TDD, we check there's no apartment created
+        self.assertEqual(Apartment.objects.count(), 0)
+        # create/add new apartment
+        apartment_1 = Apartment.objects.create(address='15 rue de la République', address_complement='3-2', city='Lyon', postal_code='69005', rental_price=1200.00, charges_price=400.00, deposit_price=2400.00)
+        # test if apartment 1 is created
         self.assertEqual(Apartment.objects.count(), 1)
+        self.assertEqual(apartment_1.address, '15 rue de la République')
