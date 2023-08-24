58f35925d37d085d8ff5cec30bdb099a0d2edf09
Lucie Pellier <lucie@luciepellier.com>
2023-08-24 16:50:19 +0200

    add path favicon

--- project_immo/project_immo/urls.py
+++ project_immo/project_immo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestion_immo.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico'))
 ]
